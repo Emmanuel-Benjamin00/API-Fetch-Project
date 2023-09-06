7e045cf8a6c5b04843c04f9e18bb13505b26d047
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-09-06 16:53:46 +0530

    title added, try catch added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-async function APIfetch(){
+async function APIfetch() {
+  try {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const result = await res.json();
-    const divcards= document.getElementById("cards");
-    for(let i=0; i<result.length; i++){
-        const a = document.createElement("div");
-        a.innerHTML = `<div class="card mb-3 h-100 shadow" style="width: 18rem;">
+    const divcards = document.getElementById("cards");
+    for (let i = 0; i < result.length; i++) {
+      const a = document.createElement("div");
+      a.innerHTML = `<div class="card mb-3 h-100 shadow" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title mb-3">Name: ${result[i].name}</h5>
           <p class="card-text">Email: ${result[i].email}</p>
@@ -12,8 +13,10 @@ async function APIfetch(){
           <p class="card-text">City: ${result[i].address.city}</p>
         </div>
       </div>`
-        divcards.append(a);
-       
+      divcards.append(a);
     }
+  } catch (error) {
+    console.log(Response.status(404))
+  }
 }
 APIfetch();
